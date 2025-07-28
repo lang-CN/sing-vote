4b4154e1889220bfd9d509b3a6fb2a41fb0880fe
langll2 <langll2@lenovo.com>
2025-07-28 18:23:19 +0800

    fix proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,14 @@
 // api/proxy.js
 // 该服务为 vercel serve跨域处理
-const { createProxyMiddleware } = require('http-proxy-middleware')
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = (req, res) => {
-    let target = ''
+export default (req, res) => {
+    let target = '';
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://gmall-h5-api.atguigu.cn
     if (req.url.startsWith('/api')) {
-        target = 'http://180.213.79.236:8180'
+        target = 'http://180.213.79.236:8180';
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
@@ -19,5 +19,5 @@ module.exports = (req, res) => {
             // 如果开启了,那么 /api/user/login 将被转发到 http://180.213.79.236:8180/user/login
             //'^/api/': '/',
         },
-    })(req, res)
-}
+    })(req, res);
+};
