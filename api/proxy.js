51c8cb60bc6e4111c75801e42962eb6de5e91ad4
langlele <langlele@noreply.gitcode.com>
2025-07-29 23:15:31 +0800

    修改 proxy

--- api/proxy.js
+++ api/proxy.js
@@ -13,15 +13,9 @@ export default (req, res) => {
         pathRewrite: {
             '^/api/': '/',
         },
-        onProxyReq: (proxyReq, req, res) => {
-            // 确保移除可能存在的旧 Authorization 头
-            proxyReq.removeHeader('Authorization');
-            // 添加新的 Authorization 头
-            proxyReq.setHeader('Authorization', 'Bearer dad60b3910423ddc915e991f38c2c0d9');
-            
-            // 调试：打印设置的请求头，确认是否正确
-            console.log('Proxy headers:', proxyReq.getHeaders());
-        },
+        headers: {
+            'Authorization': 'Bearer dad60b3910423ddc915e991f38c2c0d9'
+        }
     });
     
     // 执行代理
