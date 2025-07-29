a3e255740762a0d8aa2531f8e91ca043567f0f2d
langll2 <langll2@lenovo.com>
2025-07-29 08:26:24 +0800

    change vote-db

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ export default (req, res) => {
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://gmall-h5-api.atguigu.cn
     if (req.url.startsWith('/api')) {
-        target = 'http://180.213.79.236:8180';
+        target = 'http://vote-db.langll.cn';
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
@@ -17,7 +17,7 @@ export default (req, res) => {
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/api`
             // 如果开启了,那么 /api/user/login 将被转发到 http://180.213.79.236:8180/user/login
-            //'^/api/': '/',
+            '^/api/': '/',
         },
     })(req, res);
 };
